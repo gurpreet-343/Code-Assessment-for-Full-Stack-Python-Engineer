c7f527e02e8ecbe813f175ab0eec06879c7065b4
Gurpreet Singh <gsinghn8@gmail.com>
2023-11-06 21:10:35 +0530

    Added APIs and Test Cases for Reviews functionality

--- main.py
+++ main.py
@@ -1,15 +1,89 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query, Path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from model import Review, Tag, ReviewTag, ReviewReviewTag, Base, ReviewResponse, ReviewTagCreate, ReviewCreate, TagResponse, TagCreate
 
-app = FastAPI()
+app = FastAPI(title="Reviews Rest APIs")
+
+engine = create_engine("sqlite:///reviews.db")
+Base.metadata.create_all(bind=engine, checkfirst=True)
+SessionLocal = sessionmaker(bind=engine)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/reviews/{review_id}/tags", response_model=ReviewResponse)
+async def add_tags_to_review(
+    review_id: int,
+    review_tags: ReviewTagCreate,
+    db: Session = Depends(get_db),
+):
+    review = db.query(Review).filter(Review.id == review_id).first()
+    if not review:
+        raise HTTPException(status_code=404, detail="Review not found")
 
-# TODO Please complete the FastAPI routing here
+    for tag_id in review_tags.tag_ids:
+        tag = db.query(Tag).filter(Tag.id == tag_id).first()
+        if tag:
+            review_tag = ReviewTag(review_id=review_id, tag_id=tag_id)
+            db.add(review_tag)
+
+    db.commit()
+    db.refresh(review)
+    return review
+
+
+@app.get("/reviews", response_model=list[ReviewResponse])
+async def get_reviews(skip: int = Query(0, title="Skip records"),
+                      limit: int = Query(10, title="Limit records"),
+                      tag_ids: list[int] = Query([], title="Filter by tag IDs"),
+                      db: Session = Depends(get_db)):
+    query = db.query(Review)
+
+    if tag_ids:
+        query = query.join(ReviewReviewTag).filter(
+            ReviewReviewTag.review_tag_id.in_(tag_ids))
+
+    reviews = query.offset(skip).limit(limit).all()
+
+    return reviews
+
+
+@app.post("/reviews", response_model=ReviewResponse)
+async def create_review(review: ReviewCreate, db: Session = Depends(get_db)):
+    db_review = Review(**review.model_dump())
+    db.add(db_review)
+    db.commit()
+    db.refresh(db_review)
+    return db_review
+
+
+@app.post("/tags", response_model=TagResponse)
+async def create_tag(tag: TagCreate, db: Session = Depends(get_db)):
+    new_tag = Tag(**tag.model_dump())
+    db.add(new_tag)
+    db.commit()
+    return new_tag
+
+
+@app.delete("/tags/{tag_id}", response_model=TagResponse)
+async def delete_tag(tag_id: int = Path(..., title="Tag ID"), db: Session = Depends(get_db)):
+    tag = db.query(Tag).filter(Tag.id == tag_id).first()
+    if not tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    db.query(ReviewTag).filter(ReviewTag.tag_id == tag_id).delete()
+    db.delete(tag)
+    db.commit()
+    return tag
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- model.py
+++ model.py
@@ -1,1 +1,60 @@
-# TODO Please complete the Table definition here
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base
+from pydantic import BaseModel
+
+Base = declarative_base()
+
+# Database ORM models
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    text = Column(String(2048))
+    is_tagged = Column(Boolean)
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50))
+
+class ReviewTag(Base):
+    __tablename__ = 'review_tags'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    is_ai_tag = Column(Boolean, nullable=False, default=False)
+    tag_id = Column(Integer, ForeignKey('tags.id'))
+    review_id = Column(Integer, ForeignKey('reviews.id'))
+
+class ReviewReviewTag(Base):
+    __tablename__ = 'review_review_tags'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    review_id = Column(Integer, ForeignKey('reviews.id'))
+    review_tag_id = Column(Integer, ForeignKey('review_tags.id'))
+
+
+# Request and response models
+
+class TagCreate(BaseModel):
+    name: str
+
+
+class ReviewTagCreate(BaseModel):
+    tag_ids: list[int]
+
+
+class TagResponse(BaseModel):
+    id: int
+    name: str
+
+
+class ReviewResponse(BaseModel):
+    id: int
+    text: str
+    is_tagged: bool
+
+class ReviewCreate(BaseModel):
+    text: str
+    is_tagged: bool
